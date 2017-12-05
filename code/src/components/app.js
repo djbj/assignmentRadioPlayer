aec14c9f50516afcbe375e743cce36554a995cec
Bjorn <bjossijohnson@gmail.com>
2017-12-05 11:00:28 +0100

    Saved

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -2,14 +2,30 @@ import React from "react"
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <div>
-        Find me in src/app.js!
-      </div>
+  constructor(props) {
+    super(props)
+    this.state = {
+      channels: []
+    }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
     )
   }
 
+  render() {
+    if (this.state.channels.length > 0) {
+      return <div>Radio Channels</div>
+    } else {
+      return <div>Loading Radio Channels</div>
+    }
+
+
+
+  }
 }
 
 export default App
