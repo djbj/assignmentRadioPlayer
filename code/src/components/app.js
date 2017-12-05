e2ca5ddee0673fa06684abc9685f39d99f74c8fe
Bjorn <bjossijohnson@gmail.com>
2017-12-05 11:47:47 +0100

    One channel working

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -5,26 +5,41 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      channels: []
+      listOfChannels: []
     }
   }
 
+// http://api.sr.se/api/v2/channels?format=json&size=100
+// station name, image, and colour
+
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    )
+    fetch("http://api.sr.se/api/v2/channels?format=json&size=100").then((response) => {
+      return response.json()
+    }).then((json) => {
+      this.setState({
+        listOfChannels: json.channels
+      })
+    })
   }
 
   render() {
-    if (this.state.channels.length > 0) {
-      return <div>Radio Channels</div>
+    if (this.state.listOfChannels.length > 0) {
+      return <div>
+        Radio Channels <br/>
+        Here you can find a list of {this.state.listOfChannels.length} radio channels: <br/>
+        Channel name: {this.state.listOfChannels[0].name}<br/>
+        Image name: {this.state.listOfChannels[0].image}<br/>
+        Colour value: {this.state.listOfChannels[0].color}<br/>
+        <audio controls autoPlay>
+          <source src={this.state.listOfChannels[0].liveaudio.url} type="audio/mpeg"/>
+          <source src="" type="audio/mpeg"/>
+          Your browser does not support the audio tag.
+        </audio><br/>
+
+      </div>
     } else {
       return <div>Loading Radio Channels</div>
     }
-
-
-
   }
 }
 
